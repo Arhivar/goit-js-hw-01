90a34c253364d5b2c57b75393d51ed1529359b07
Arhivar <artyr25072001@gmail.com>
2023-12-13 11:32:16 -0500

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
 "use strict";
-function makeTransaction (quantity, pricePerDroid) {
- return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`
+function makeTransaction(quantity, pricePerDroid) {
+  let totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500))
+console.log(makeTransaction(10, 500));
